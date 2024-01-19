7c0eccd5f01882b2612bf0fe88ecde7c1dcec1c8
AnkitMehta <ankitsingmehta12345@gmail.com>
2024-01-19 14:19:04 +0530

    Javascript

---
+++ JS CODE/1alert.js
@@ -0,0 +1,15 @@
+// alert in JavaScript is used to display a pop-up dialog box with a specified message, providing a simple way to communicate information to users in the browser.
+alert("hello world")
+
+
+
+// document.write in JavaScript is used to dynamically insert content into HTML documents during page load, but it's not commonly recommended due to potential drawbacks, such as overwriting the entire document and limited use in modern web development.
+document.write("welcome to ankit page")
+
+
+console.log("hello world");
+// open browser
+// right click
+// select inspect
+//  click on console
+// ouput-hello world
---
+++ JS CODE/2variable_injs.js
@@ -0,0 +1,9 @@
+// In JavaScript, a variable is a symbolic name for a value, allowing developers to store and manipulate data. Variables are declared using var, let, or const, each with different scoping and mutability characteristics.
+// var   es5
+
+// let   es6
+// const  es6
+
+var x=30
+var x="hello world"
+console.log(x);
