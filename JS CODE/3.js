bb08b5f9b28904f03a196e538d03b420aaed3ccd
ankitsinghmehta2 <ankitsingmehta@12345@gmail.com>
2024-02-19 20:23:34 +0530

    code

---
+++ JS CODE/3.js
@@ -0,0 +1,21 @@
+// For Loop
+console.log("For Loop:");
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// While Loop
+console.log("\nWhile Loop:");
+let j = 0;
+while (j < 5) {
+    console.log(j);
+    j++;
+}
+
+// Do-While Loop
+console.log("\nDo-While Loop:");
+let k = 0;
+do {
+    console.log(k);
+    k++;
+} while (k < 5);
