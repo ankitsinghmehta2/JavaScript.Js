2ad67db948b90719b26710cbc201134eae90303c
Ankit Mehta <126945410+ankitsinghmehta2@users.noreply.github.com>
2024-02-22 13:58:30 +0530

    Update 1alert.js

--- JS CODE/1alert.js
+++ JS CODE/1alert.js
@@ -6,7 +6,6 @@ alert("hello world")
 // document.write in JavaScript is used to dynamically insert content into HTML documents during page load, but it's not commonly recommended due to potential drawbacks, such as overwriting the entire document and limited use in modern web development.
 document.write("welcome to ankit page")
 
-
 console.log("hello world");
 // open browser
 // right click
